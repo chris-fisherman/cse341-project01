a512da9a3937d02b29e3f0d8b0e385b9eba35ab2
Christian Peraza <christianperaza2000@gmail.com>
2025-01-11 13:38:22 -0300

    Connect node project to MongoDB
    Create an .env file to store MongoDB connection string. Install mongo and dotenv packages. And connect project to MongoDB.

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const routes = require('./routes')
+const mongodb = require('./data/database')
 
 app.use('/', routes)
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}.`)
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err)
+    } 
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node running on port ${port}.`)
+        })
+    }
 })
