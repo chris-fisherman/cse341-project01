a326d04e7db32da7fc8ef077f6c36387ca8306b9
Christian Peraza <christianperaza2000@gmail.com>
2025-01-11 12:05:07 -0300

    Create server and first route

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+/****************************/
+/*** REQUIRE STATEMENTS ***/
+/****************************/
+const express = require('express')
+const router = express()
+
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,13 @@
+/****************************/
+/*** REQUIRE STATEMENTS ***/
+/****************************/
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const routes = require('./routes')
+
+app.use('/', routes)
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}.`)
+})
